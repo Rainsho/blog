584816ecb6ea04b344a39fd6d9484ad515a3d95c
Rainsho <djkyux@163.com>
2019-07-16 23:44:20 +0800

    update scture

--- codes/algorithm/LRU.js
+++ codes/algorithm/LRU.js
@@ -1,6 +1,6 @@
 /**
  * LRU (Least Recently Used) 最近最少使用策略
- * 使用 双向链表 数据结构
+ * 使用 散列表 + 双向链表 O(1) 复杂度实现
  */
 
 class Node {
@@ -8,93 +8,104 @@ class Node {
     this.val = val;
     this.prev = null;
     this.next = null;
+    this.hnext = null;
+  }
+}
+
+class HashTable {
+  constructor(capacity = 10) {
+    this.capacity = capacity;
+    this.table = {};
+  }
+
+  // range(0, 5)
+  hashed(val) {
+    if (!val) return 0;
+
+    return (
+      val
+        .split('')
+        .map(x => x.codePointAt(0))
+        .reduce((sum, cur) => sum + cur, 0) % 5
+    );
+  }
+
+  add(val) {
+    const node = new Node(val);
+    const hash = this.hashed(node.val);
+
+    if (!this.table[hash]) {
+      this.table[hash] = node;
+      return val;
+    }
+
+    let last = this.table[hash];
+
+    while (last.hnext) last = last.hnext;
+
+    last.hnext = node;
+
+    return val;
+  }
+
+  remove(val) {
+    const hash = this.hashed(val);
+
+    if (!this.table[hash]) return false;
+
+    let current = this.table[hash];
+
+    if (current.val === val) {
+      this.table[hash] = current.hnext;
+      return true;
+    }
+
+    while (current.val !== val && current.hnext) {
+      if (current.hnext.val === val) {
+        current.hnext = current.hnext.hnext;
+        return true;
+      }
+
+      current = current.hnext;
+    }
+
+    return false;
+  }
+
+  has(val) {
+    const hash = this.hashed(val);
+
+    if (!this.table[hash]) return false;
+
+    let current = this.table[hash];
+
+    if (current.val === val) return true;
+
+    while (current.val !== val && current.hnext) {
+      if (current.hnext.val === val) return true;
+
+      current = current.hnext;
+    }
+
+    return false;
+  }
+
+  toString() {
+    return Object.keys(this.table)
+      .map(key => {
+        const values = [];
+        let current = this.table[key];
+
+        while (current) {
+          values.push(current.val);
+          current = current.hnext;
+        }
+
+        return `${key}: ${values.join(' -> ')}`;
+      })
+      .join('\n');
   }
 }
 
 // prettier-ignore
 const genStr = () => Math.random().toString(36).substr(2, 8);
-
-const THRESHOLD = 5;
-const visitTimes = THRESHOLD * 3;
-const fragments = new Array(THRESHOLD * 2).fill(1).map(genStr);
-const first = new Node('$$$');
-
-function visitor(val, current = first, i = 0) {
-  // delete last and insert a new node
-  if (i === THRESHOLD) {
-    current.prev.next = null;
-
-    const second = new Node(val);
-
-    second.prev = first;
-    second.next = first.next;
-    first.next.prev = second;
-    first.next = second;
-
-    return;
-  }
-
-  // put current to first
-  if (current.val === val) {
-    if (current.prev) {
-      current.prev.next = current.next;
-    }
-
-    if (current.next) {
-      current.next.prev = current.prev;
-    }
-
-    current.prev = first;
-    current.next = first.next;
-
-    if (first.next) {
-      first.next.prev = current;
-    }
-
-    first.next = current;
-
-    return;
-  }
-
-  // insert a new node
-  if (current.next === null) {
-    const second = new Node(val);
-
-    second.prev = first;
-    second.next = first.next;
-
-    if (first.next) {
-      first.next.prev = second;
-    }
-
-    first.next = second;
-
-    return;
-  }
-
-  visitor(val, current.next, i + 1);
-}
-
-function printList(val) {
-  const lists = [];
-
-  function getVal(current = first) {
-    lists.push(current.val);
-
-    if (current.next) getVal(current.next);
-  }
-
-  getVal();
-
-  console.log('visit', val);
-  console.log(lists.join(' -> '));
-}
-
-// start visting
-new Array(visitTimes).fill(1).forEach(() => {
-  const i = ~~(Math.random() * fragments.length);
-  const val = fragments[i];
-
-  visitor(val);
-  printList(val);
-});
